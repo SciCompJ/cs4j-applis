b14d3912d1a050d974e64c13a5dde6633cb2c8ab
dlegland <david.legland@inrae.fr>
2020-10-23 18:05:22 +0200

    add classes for normalizing the (bright) background

---
+++ fasga/src/main/java/cs4j/fasga/NormalizeBackground.java
@@ -0,0 +1,218 @@
+/**
+ * 
+ */
+package cs4j.fasga;
+
+import java.io.File;
+import java.io.IOException;
+
+import net.sci.array.Array2D;
+import net.sci.array.color.RGB8Array2D;
+import net.sci.array.scalar.BinaryArray2D;
+import net.sci.array.scalar.ScalarArray2D;
+import net.sci.array.scalar.UInt8Array;
+import net.sci.array.scalar.UInt8Array2D;
+import net.sci.image.Image;
+import net.sci.image.morphology.MorphologicalReconstruction;
+import net.sci.image.process.segment.OtsuThreshold;
+
+/**
+ * @author dlegland
+ *
+ */
+public class NormalizeBackground
+{
+    public static final BinaryArray2D segmentImage_RGB8(RGB8Array2D array)
+    {
+        System.out.println("Conversion toUInt8");
+        UInt8Array2D gray8 = array.convertToUInt8();
+        
+        System.out.println("Segmentation using Otsu Threshold");
+        BinaryArray2D segStem = (BinaryArray2D) new OtsuThreshold().processScalar(gray8);
+        
+        // Fill holes, and keep mask of the background
+        segStem = segStem.complement();
+        segStem = (BinaryArray2D) MorphologicalReconstruction.fillHoles(segStem);
+        segStem = segStem.complement();
+        
+        return segStem;
+    }
+    
+    /**
+     * Normalizes the background of the input image, assuming the structures are
+     * dark over a bright background, and that the background can be modelled
+     * with a polynomial of the coordinates.
+     * 
+     * @param image
+     *            the image to normalize (either UInt8 or RGB8)
+     * @param mask
+     *            the binary mask of the background
+     * @param maxDegree
+     *            the maximal degree of the fitting polynomial
+     * @return an array the same class as the input array, normalized by the
+     *         background estimate.
+     */
+    public final static Array2D<?> normalizeBackground(Array2D<?> image, BinaryArray2D mask, int maxDegree)
+    {
+        if (image instanceof UInt8Array2D)
+        {
+            return normalizeBackground_UInt8((UInt8Array2D) image, mask, maxDegree);
+        }
+        else if (image instanceof RGB8Array2D)
+        {
+            return normalizeBackground_RGB8((RGB8Array2D) image, mask, maxDegree);
+        }
+        else
+        {
+            throw new IllegalArgumentException("Unable to handle array with class: " + image.getClass().getName());
+        }
+    }
+    
+    /**
+     * Computes background model from image and mask, with the specified degree,
+     * and computes normalized ratio of input image with background. 
+     */
+    public final static RGB8Array2D normalizeBackground_RGB8(RGB8Array2D image, BinaryArray2D mask, int maxDegree)
+    {
+        RGB8Array2D res = RGB8Array2D.create(image.size(0), image.size(1));
+        
+        // iterate over channels
+        for (int c = 0; c < 3; c++)
+        {
+            UInt8Array2D channel = image.channel(c);
+            UInt8Array2D channel2 = normalizeBackground_UInt8(channel, mask, maxDegree);
+            res.setChannel(c, channel2);
+        }
+        return res;
+    }
+
+    public final static UInt8Array2D normalizeBackground_UInt8(UInt8Array2D image, BinaryArray2D mask, int maxDegree)
+    {
+        ScalarArray2D<?> bgFit = fitBackground(image, mask, 2, 4);
+        return normalizeBrightBackground(image, bgFit, 0.0, 1.0);
+    }
+    
+    /**
+     * Estimates a background image by fitting a polynomial model of the values
+     * of the input image within the mask.
+     * 
+     * @param image
+     *            the image used to estimate background model
+     * @param mask
+     *            the binary mask for values to estimate
+     * @param maxDegree
+     *            the maximum degree of the polynomial (2 or 3 is often
+     *            sufficient)
+     * @param samplingStep
+     *            can be used to reduce the computation time on large images
+     *            (typical value is 2)
+     * @return a new image the same size as the input image containing estimate
+     *         of the background value for each pixel
+     */
+    public final static ScalarArray2D<?> fitBackground(UInt8Array2D image, BinaryArray2D mask, int maxDegree, int samplingStep)
+    {
+        PolynomialBackground pbg = new PolynomialBackground(maxDegree);
+        return pbg.fitBackground(image, mask, samplingStep);
+    }
+    
+    /**
+     * Computes normalized division on scalar images. The ratio of the two images
+     * is expected to be within 0 and 1. The bounds can be adjusted to enhance
+     * the contrast of the resulting image.
+     * 
+     * @param image
+     *            the image to normalize
+     * @param bgImage
+     *            A scalar image processor containing the estimate of the
+     *            (bright) background
+     * @param lower
+     *            the lower bound of the result that will be mapped to 0
+     * @param upper
+     *            the upper bound of the result that will be mapped to 255
+     * @return a new instance of UInt8Array2D containing normalized result of
+     *         division
+     */
+    public final static UInt8Array2D normalizeBrightBackground(UInt8Array2D image,
+            ScalarArray2D<?> bgImage, double lower, double upper)
+    {
+        int sizeX = image.size(0);
+        int sizeY = image.size(1);
+        UInt8Array2D result = UInt8Array2D.create(sizeX, sizeY);
+
+        double extent = upper - lower;
+        double val1, val2, res;
+
+        // Iterate on image pixels, and choose result value depending on mask
+        for (int x = 0; x < sizeX; x++)
+        {
+            for (int y = 0; y < sizeY; y++)
+            {
+                val1 = image.getValue(x, y);
+                val2 = bgImage.getValue(x, y);
+
+                // compute normalized ratio
+                res = 255 * ((val1 / val2) - lower) / extent;
+                res = Math.max(Math.min(res, 255), 0);
+
+                result.setInt(x, y, (int) res);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * @param args
+     * @throws IOException 
+     */
+    public static void main(String[] args) throws IOException
+    {
+        System.out.println("Check current dir: " + new File(".").getCanonicalPath());
+        
+        // check file
+        File inputFile = new File("./files/maize/6635b.jpg");
+        System.out.println("  file exists: " + inputFile.exists());
+        
+        // Read image data
+        System.out.println("Read input image");
+        Image image = Image.readImage(inputFile);
+        RGB8Array2D array = (RGB8Array2D) image.getData();
+        
+        int sizeX = array.size(0);
+        int sizeY = array.size(1);
+        System.out.println("  image size: " + sizeX + "x" + sizeY);
+        
+        UInt8Array2D arrayG8 = array.convertToUInt8();
+        
+        
+        BinaryArray2D segStem = segmentImage_RGB8(array);
+
+        System.out.println("Create segmented image");
+        Image segStemImage = new Image(segStem);
+        segStemImage.setName("Segmented");
+        segStemImage.show();
+        
+        ScalarArray2D<?> bgFit = fitBackground(arrayG8, segStem, 2, 4);
+        UInt8Array2D bgFitG8 = UInt8Array2D.wrap(UInt8Array.convert(bgFit));
+        
+        Image bgImage = new Image(bgFitG8);
+        bgImage.setName("Background Estimate");
+        bgImage.show();
+//        System.out.println("Save mask image 'segStem.png'");
+//        File outputFile = new File("./files/segStem.png");
+//        System.out.println("  segStem output File: " + outputFile.getAbsolutePath());
+//        ImageWriter writer = new ImageIOImageWriter(outputFile);
+//        writer.writeImage(segStemImage);
+
+        UInt8Array2D arrayG8N = normalizeBrightBackground(arrayG8, bgFit, 0, 1);
+        Image resImageG8 = new Image(arrayG8N);
+        resImageG8.setName("Normalized");
+        resImageG8.show();
+        
+        
+        RGB8Array2D normalized = normalizeBackground_RGB8(array, segStem, 2);
+        Image normalizedImage = new Image(normalized);
+        normalizedImage.setName("RGB8 Normalized");
+        normalizedImage.show();
+    }
+}
---
+++ fasga/src/main/java/cs4j/fasga/PolynomialBackground.java
@@ -0,0 +1,260 @@
+/**
+ * 
+ */
+package cs4j.fasga;
+
+import Jama.Matrix;
+import Jama.QRDecomposition;
+import net.sci.array.scalar.BinaryArray2D;
+import net.sci.array.scalar.Float32Array2D;
+import net.sci.array.scalar.ScalarArray2D;
+import net.sci.array.scalar.UInt8Array2D;
+
+/**
+ * Fit a polynomial surface to the background of an image.
+ * 
+ * @author dlegland
+ */
+public class PolynomialBackground
+{
+    int maxOrder;
+    double[] coeffs;
+    
+    int[] xPowers;
+    int[] yPowers;
+    
+    double[][] xMonoms;
+    double[][] yMonoms;
+    
+    
+    /**
+     * Constructs a new polynomial background fitter with the given order.
+     * 
+     * @param maxOrder
+     *            the maximal order of x and y coefficients
+     */
+    public PolynomialBackground(int maxOrder)
+    {
+        this.maxOrder = maxOrder;
+        initXYPowers();
+    }
+
+    public void sertMaxOrder(int maxOrder)
+    {
+        this.maxOrder = maxOrder;
+        initXYPowers();
+    }
+
+    /**
+     * Compute x and y powers depending on the polynomial order. Also allocates
+     * memory for coefficients.
+     */
+    private void initXYPowers()
+    {
+        // computes number of coefficients from max maxOrder.
+        // Uses Gauss's relation
+        int nCoeffs = (this.maxOrder + 1) * (this.maxOrder + 2) / 2;
+
+        // allocate memory for coefficient array
+        this.coeffs = new double[nCoeffs];
+
+        // powers of x and y
+        this.xPowers = new int[nCoeffs];
+        this.yPowers = new int[nCoeffs];
+
+        // initialize the arrays of x and y powers
+        int c = 1;
+        for (int order = 1; order <= this.maxOrder; order++)
+        {
+            for (int n = 0; n <= order; n++)
+            {
+                this.xPowers[c] = order - n;
+                this.yPowers[c] = n;
+                c++;
+            }
+        }
+    }
+    
+
+    /**
+     * Estimate the background coefficient from a given couple image+mask, and
+     * returns the estimated background image.
+     * 
+     * @param image input image containing the background and the foreground
+     * @param mask binary image corresponding to the background
+     * @param samplingStep sampling ratio for estimating background (1 = all pixels)
+     * @return an estimate of the background image 
+     */
+    public ScalarArray2D<?> fitBackground(UInt8Array2D image, BinaryArray2D mask, int samplingStep) {
+//      double t0 = System.currentTimeMillis();
+        // Fit background coefficients
+        fitCoefficients(image, mask, samplingStep);
+//      double t1 = System.currentTimeMillis();
+//      System.out.println("Fit coeffs.:   " + (t1 - t0) + " ms");
+        
+        // image dimensions
+        int sizeX = image.size(0);
+        int sizeY = image.size(1);
+        Float32Array2D result;
+        try 
+        {
+            result = Float32Array2D.create(sizeX, sizeY);
+        }
+        catch (Exception ex) 
+        {
+            throw new RuntimeException("Could not create background image");
+        }
+
+//      double t2 = System.currentTimeMillis();
+//      System.out.println("Create image:  " + (t2 - t1) + " ms");
+        
+        fillImage(result);
+//      double t3 = System.currentTimeMillis();
+//      System.out.println("Fill:          " + (t3 - t2) + " ms");
+
+        return result;
+    }
+    
+    
+    public void fitCoefficients(UInt8Array2D image, BinaryArray2D mask, int samplingStep)
+    {
+        // number of coefficients
+        int nCoeffs = this.coeffs.length;
+
+        // image dimensions
+        int sizeX = image.size(0);
+        int sizeY = image.size(1);
+
+        // first count the number of points within the mask,
+        // in order to known the size of the matrix
+        int nValues = 0;
+        for (int y = 0; y < sizeY; y += samplingStep)
+        {
+            for (int x = 0; x < sizeX; x += samplingStep)
+            {
+                if (mask.getBoolean(x, y))
+                    nValues++;
+            }
+        }
+
+        // pre-compute x and y monoms
+        computeMonoms(sizeX, sizeY);
+
+        // initialize matrices A and b
+        Matrix matrix, values;
+        try
+        {
+            matrix = new Matrix(nValues, nCoeffs);
+            values = new Matrix(nValues, 1);
+        } catch (Exception ex)
+        {
+            throw new RuntimeException(
+                    "Could not allocate memory for matrix with " + nValues
+                            + " values and " + nCoeffs + " coefficients");
+        }
+
+        // iterate over each pixel to initialize both matrix and vector of
+        // values
+        int i = 0;
+        for (int y = 0; y < sizeY; y += samplingStep)
+        {
+            for (int x = 0; x < sizeX; x += samplingStep)
+            {
+                // do not process values outside mask
+                if (!mask.getBoolean(x, y))
+                    continue;
+
+                // init matrix
+                for (int c = 0; c < nCoeffs; c++)
+                {
+                    matrix.set(i, c, xMonoms[x][c] * yMonoms[y][c]);
+                }
+
+                // init vector of values to fit
+                values.set(i, 0, image.getValue(x, y));
+
+                i++;
+            }
+        }
+
+        // find least squares solution
+        QRDecomposition qr = new QRDecomposition(matrix);
+        Matrix theta = qr.solve(values);
+
+        // extract coefficients
+        for (int c = 0; c < nCoeffs; c++)
+            coeffs[c] = theta.get(c, 0);
+    }
+
+    /**
+     * Fills the given image processor with the background estimate of each 
+     * position.
+     *  
+     * @param image
+     */
+    public void fillImage(ScalarArray2D<?> image) {
+        int nCoeffs = coeffs.length;
+        
+        // image dimensions
+        int sizeX = image.size(0);
+        int sizeY = image.size(1);
+        
+        computeMonoms(sizeX, sizeY);
+        
+        // iterate over each pixel to initialize both matrix and vector of values
+        for (int y = 0; y < sizeY; y++)
+        {
+            for (int x = 0; x < sizeX; x++)
+            {
+                // compute value at current position
+                double value = 0;
+                for (int c = 0; c < nCoeffs; c++)
+                {
+                    value += coeffs[c] * xMonoms[x][c] * yMonoms[y][c]; 
+                }
+                
+                image.setValue(x, y, value);
+            }
+        }
+    }
+
+    /**
+     * Pre-computes the monoms used for fitting the polynomial function.
+     * 
+     * @param sizeX
+     *            the size of the array in the first direction
+     * @param sizeY
+     *            the size of the array in the second direction
+     */
+    private void computeMonoms(int sizeX, int sizeY)
+    {
+        int nCoeffs = coeffs.length;
+    
+        xMonoms = new double[sizeX][nCoeffs];
+        for (int x = 0; x < sizeX; x++)
+        {
+            double x2 = normalizePosition(x, sizeX);
+            for (int c = 0; c < nCoeffs; c++)
+                xMonoms[x][c] = Math.pow(x2, xPowers[c]);
+        }
+    
+        yMonoms = new double[sizeY][nCoeffs];
+        for (int y = 0; y < sizeY; y++)
+        {
+            double y2 = normalizePosition(y, sizeY);
+            for (int c = 0; c < nCoeffs; c++)
+                yMonoms[y][c] = Math.pow(y2, yPowers[c]);
+        }
+    }
+
+    /**
+     * Converts a position given as an index between 0 and a given size, into a
+     * position between -1.5 and +1.5, better suited for numerical computations
+     * involving powers.
+     */
+    private static double normalizePosition(int index, int size)
+    {
+        return (((double) index / (double) size) - .5) * 3;
+    }
+    
+}
